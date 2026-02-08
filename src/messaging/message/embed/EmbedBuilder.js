73df721859d7f4bb57e9ea180acce47f5fe30a81
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-02-08 17:13:10 -0400

    refactor: reorganize EmbedBuilder methods for improved structure and readability

--- src/messaging/message/embed/EmbedBuilder.js
+++ src/messaging/message/embed/EmbedBuilder.js
@@ -1,33 +1,48 @@
 
 export default class EmbedBuilder {
-	#embed = {};
+  #embed = {};
+  #fields = [];
 
-	setTitle(title) {
-		this.#embed.title = title;
-		return this;
-	}
+  setTitle(title) {
+    this.#embed.title = title;
+    return this;
+  }
 
-	setDescription(description) {
-		this.#embed.description = description;
-		return this;
-	}
+  setDescription(description) {
+    this.#embed.description = description;
+    return this;
+  }
 
-	setImage(url) {
-		this.#embed.image = { url };
-		return this;
-	}
+  setImage(url) {
+    this.#embed.image = { url };
+    return this;
+  }
 
-	setThumbnail(url) {
-		this.#embed.thumbnail = { url };
-		return this;
-	}
+  setThumbnail(url) {
+    this.#embed.thumbnail = { url };
+    return this;
+  }
 
-	setFooter(text) {
-		this.#embed.footer = { text };
-		return this;
-	}
+  setColor(color) {
+    this.#embed.color = color;
+    return this;
+  }
 
-	build() {
-		return structuredClone(this.#embed);
-	}
+  setFooter(text) {
+    this.#embed.footer = { text };
+    return this;
+  }
+
+  addField(name, value, inline = false) {
+    this.#fields.push({ name, value, inline });
+    return this;
+  }
+
+  build() {
+    if (this.#fields.length > 0) {
+      this.#embed.fields = this.#fields;
+    }
+
+    return structuredClone(this.#embed);
+  }
 }
