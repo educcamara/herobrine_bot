b59a4d423f38956af45801a96565fc0cac9b9823
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-02-08 16:56:58 -0400

    feat: add MessageBuilder class for constructing messages with text and embeds

---
+++ src/messaging/message/MessageBuilder.js
@@ -0,0 +1,27 @@
+import Message from "./Message";
+
+
+export default class MessageBuilder {
+	#content = null;
+	#embeds = [];
+
+	addText(text) {
+		this.#content = text;
+		return this;
+	}
+
+	addEmbed(configure) {
+		const embedBuilder = new EmbedBuilder();
+		configure(embedBuilder);
+
+		this.#embeds.push(embedBuilder.build());
+		return this;
+	}
+
+	build() {
+		return new Message({
+			content: this.#content,
+			embeds: this.#embeds
+		});
+	}
+}
