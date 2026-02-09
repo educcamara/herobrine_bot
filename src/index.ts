8aa1bc1f392ebc7933c1233d883d1603b48d7868
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-02-09 11:29:00 -0400

    fix: import client from bot module in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import config from "../config.json" with { type: "json" };
 import logger from "./infrastructure/logging/logger.js";
+import client from "./bot/bot.js";
 
 client.login(config.discord_token).catch(error => {
   logger.error("Failed to login", error);
